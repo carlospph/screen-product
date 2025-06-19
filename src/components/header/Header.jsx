eb13adbd85599e31512c031a9c7f20af4db8d141
Carlos Nascimento <80932010+carlospph@users.noreply.github.com>
2025-06-19 09:25:02 -0300

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,1 +1,15 @@
+import React from "react";
+import styles from "./Header.module.css";
 
+export function Header() {
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <i className="fa-solid fa-terminal"></i>
+          <span>Store tech</span>
+        </div>
+      </div>
+    </>
+  );
+}
